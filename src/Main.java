2c1efe7b3e017b63779754c76910be00d8efeb5c
Matia Fazio <matia.fazio@gmail.com>
2021-01-05 11:56:58 +0100

    Rivisitazioni al progetto, aggiunta di tasto chiusura alla dashboard e rimozione della top bar, con estensione dell'area clicabile per il drag a tutta la finestra

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,37 @@
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
+    private double x, y;
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(Main.class.getResource("/view/registrationClient.fxml"));
-        stage.setTitle("App Clinic");
-        stage.setScene(new Scene(root, 800, 600));
+        Parent root = FXMLLoader.load(Main.class.getResource("/view/doctorDashboard.fxml"));
+
+        root.setOnMousePressed(mouseEvent -> {
+            x = mouseEvent.getSceneX();
+            y = mouseEvent.getSceneY();
+        });
+
+        root.setOnMouseDragged(mouseEvent -> {
+            stage.setX(mouseEvent.getScreenX() - x);
+            stage.setY(mouseEvent.getScreenY() - y);
+        });
+
+        stage.setScene(new Scene(root));
+        stage.initStyle(StageStyle.TRANSPARENT);
         stage.show();
         stage.setResizable(false);
     }
+
     public static void main(String[] args) {
         launch(args);
     }
--- src/controller/DoctorDashboard.java
+++ src/controller/DoctorDashboard.java
@@ -1,4 +1,89 @@
 package controller;
 
-public class DoctorDashboard {
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.effect.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import static javafx.scene.input.KeyCode.R;
+
+public class DoctorDashboard implements Initializable{
+    public VBox sidebar;
+    public Button pazienti = new Button("Pazienti");
+    public Button agenda = new Button("Agenda");
+    public BorderPane borderPane;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.setButtons(sidebar, pazienti, agenda);
+    }
+
+    public void setButtons (VBox vBox, Button... buttons) {
+        for (Button button: buttons) {
+            DropShadow shadow = new DropShadow();
+            Blend lighting = new Blend();
+
+            //Adding the shadow when the mouse cursor is on
+            button.addEventHandler(MouseEvent.MOUSE_ENTERED,
+                    e -> button.setEffect(shadow));
+            //Removing the shadow when the mouse cursor is off
+            button.addEventHandler(MouseEvent.MOUSE_EXITED,
+                    e -> button.setEffect(null));
+
+            button.addEventHandler(MouseEvent.MOUSE_CLICKED,
+                    e -> button.setEffect(lighting));
+
+            button.setStyle("-fx-background-color: #3DA4E3; -fx-background-radius: 30px, 30px, 30px, 30px;"); //ffffff00 transparent
+            button.setFont(Font.font("Bauhaus 93", 20.0));
+            button.setMnemonicParsing(false);
+            button.setPrefWidth(134.0);
+            button.setPrefHeight(25.0);
+            button.setTextFill(Paint.valueOf("WHITE"));
+
+            // Qua voglio fare che se il bottone da creare è "pazienti" allora mi crea un tabpane con 2 tab,
+            // una con la lista dei clienti e una dove inserirne uno nuovo
+
+//            if (button.getText().equals("Pazienti")) {
+//                TabPane tabPane = new TabPane();
+//                Tab pazienti = new Tab("Pazienti");
+//                pazienti.
+//                Tab nuovoPaziente = new Tab("Nuovo Paziente");
+//                tabPane.getTabs().add(pazienti);
+//                tabPane.getTabs().add(nuovoPaziente);
+//                borderPane.setCenter();
+//            }
+            button.setOnMouseClicked(e -> {
+                Parent root = null;
+                try {
+                    root = FXMLLoader.load(getClass().getResource("/view/" + button.getText().toLowerCase(Locale.ROOT) + ".fxml"));
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+
+                borderPane.setCenter(root);
+            });
+
+            button.setId(button.getText().toLowerCase(Locale.ROOT));
+            vBox.getChildren().add(button);
+        }
+        vBox.setSpacing(3.0);
+    }
+
+    public void close() {
+        Stage stage = (Stage) borderPane.getScene().getWindow();
+        stage.close();
+    }
 }
