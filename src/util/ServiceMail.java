57a7b7a1e6e79f270015a97b443a8f295fc7eeff
Ylenia Galluzzo <yleniagalluzzo1@gmail.com>
2021-01-13 22:15:47 +0100

    aggiunta progress bar nell'invio email

--- src/util/ServiceMail.java
+++ src/util/ServiceMail.java
@@ -5,6 +5,7 @@ import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.swing.*;
+import java.awt.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Properties;
@@ -13,13 +14,42 @@ import java.util.logging.Logger;
 
 
 public class ServiceMail {
+    private static JFrame jfrProgress;
+    private static Container contentPane;
+    private static JProgressBar progressBar;
+    private static String doing;
     public static void sendMail(String receiver, LocalDate dataVisit, LocalTime timeStartVisit, LocalTime timeEndVisit) {
 
         System.out.println("Prepazione invio email in corso... ");
-        Alert alert = new Alert(Alert.AlertType.INFORMATION,
+        /*Alert alert = new Alert(Alert.AlertType.INFORMATION,
                 "Prepazione invio email in corso...");
         alert.setTitle("Prepazione invio email in corso...!");
-        alert.showAndWait();
+        alert.showAndWait(); */
+
+       // Make New Frame For Bar
+        jfrProgress = new JFrame("Invio email in corso......");
+        contentPane = jfrProgress.getContentPane();
+        contentPane.setLayout(null);
+        jfrProgress.setLocation(700,350);
+        jfrProgress.setSize(600,150);
+        jfrProgress.setVisible(true);
+        // Tries To Make Bar Close On Exit But Bar Sometimes Too Fast
+        jfrProgress.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Bar Progress Counter
+        int progress = 100;
+        // Make Bar
+        progressBar = new JProgressBar(0, progress);
+        doing = "Invio email in corso... 0 %";
+        jfrProgress.getContentPane().add(progressBar,BorderLayout.CENTER);
+        progressBar.setValue(0);
+        progressBar.setStringPainted(true);
+        progressBar.setString(doing);
+        progressBar.setSize(600, 150);
+        progressBar.setVisible(true);
+        progressBar.setValue(0);
+        progressBar.setStringPainted(true);
+        progressBar.paintImmediately(0,0,jfrProgress.getWidth(),jfrProgress.getHeight());
+
 
         Properties proprieties = new Properties();
 
@@ -46,9 +76,26 @@ public class ServiceMail {
 
         Message message = prepareMessage(session, myAccountEmail, receiver, dataVisit, timeStartVisit, timeEndVisit);
         try{
+            // Progress ++;
+            progressBar.setValue(66);
+            doing = "Invio email in corso... 66 %";
+            progressBar.setString(doing);
+            progressBar.paintImmediately(0,0,jfrProgress.getWidth(),jfrProgress.getHeight());
 
             Transport.send(message);
-            JOptionPane.showMessageDialog(null ,"L'email è stata inviata correttamente!");
+
+            // Progress ++;
+            progressBar.setValue(100);
+            progressBar.paintImmediately(0,0,jfrProgress.getWidth(),jfrProgress.getHeight());
+
+            //Close
+            jfrProgress.dispose();
+            //JOptionPane.showMessageDialog(null ,"L'email è stata inviata correttamente!");
+            Alert alert = new Alert(Alert.AlertType.INFORMATION,
+                    "Email Inviata!");
+            alert.setTitle("Email correttamente inviata");
+            alert.showAndWait();
+
             System.out.println("Inviata!");
         }catch (MessagingException ex){
             JOptionPane.showMessageDialog(null ,"Errore: L'email non è stata inviata!");
@@ -58,6 +105,11 @@ public class ServiceMail {
 
     private static Message prepareMessage(Session session, String myAccountEmail, String receiver, LocalDate dataVisit, LocalTime timeStartVisit, LocalTime timeEndVisit){
         try {
+            // Progress ++;
+            progressBar.setValue(33);
+            doing = "Invio email in corso... 33%";
+            progressBar.setString(doing);
+            progressBar.paintImmediately(0,0,jfrProgress.getWidth(),jfrProgress.getHeight());
             Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress(myAccountEmail));
             message.setRecipient(Message.RecipientType.TO, new InternetAddress(receiver));
