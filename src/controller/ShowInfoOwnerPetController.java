3b74f3555574288195f9be0e25fd114e33410b7a
Ylenia Galluzzo <yleniagalluzzo1@gmail.com>
2021-01-15 19:58:26 +0100

    dettagli prenotazione work in progress

---
+++ src/controller/ShowInfoOwnerPetController.java
@@ -0,0 +1,99 @@
+package controller;
+
+import dao.ConcreteAppointmentDAO;
+import datasource.ConnectionDBH2;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import model.Appointment;
+import model.Owner;
+import model.Pet;
+
+import javax.swing.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ShowInfoOwnerPetController implements Initializable {
+
+    public Label nameOwner;
+    public Label surnameOwner;
+    public Label sexOwner;
+    public Label datebirthOwner;
+    public Label addressOwner;
+    public Label cityOwner;
+    public Label codFiscalOwner;
+    public Label emailOwner;
+    public Label namePet;
+    public Label surnamePet;
+    public Label sexPet;
+    public Label datebirthPet;
+    public Label racePet;
+    public Label particularSignPet;
+    private final String idOwner;
+    private final String idPet;
+    private ConcreteAppointmentDAO appointmentRepo;
+
+    public ShowInfoOwnerPetController(Appointment appointment) {
+        this.idOwner = appointment.getId_owner();
+        this.idPet = appointment.getId_pet();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try{
+            ConnectionDBH2 connection = new ConnectionDBH2();
+            this.appointmentRepo = new ConcreteAppointmentDAO(connection);
+
+            //ricerca di dei dati del owner
+           Owner owner =  this.appointmentRepo.searchOwnerById(idOwner);
+            if(owner!=null){
+                setFieldDataOwner(owner);
+            }else
+            {
+                JOptionPane.showMessageDialog(null,"Errore nel caricamento dei dati del Cliente");
+            }
+
+            //ricerca di dei dati del Pet
+            Pet pet = this.appointmentRepo.searchPetById(idPet);
+            if(pet!=null){
+                setFieldDataPet(pet);
+            }else
+            {
+                JOptionPane.showMessageDialog(null,"Errore nel caricamento dei dati del Paziente");
+            }
+
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Error" + e.getMessage());
+        }
+    }
+
+    private void setFieldDataPet(Pet pet) {
+        namePet.setText(pet.getName());
+        surnamePet.setText(pet.getSurname());
+        sexPet.setText(pet.getSex().toString());
+        datebirthPet.setText(pet.getDatebirth().toString());
+        racePet.setText(pet.getId_petRace());
+        particularSignPet.setText(pet.getParticularSign());
+    }
+
+    private void setFieldDataOwner(Owner owner) {
+        nameOwner.setText(owner.getName());
+        surnameOwner.setText(owner.getSurname());
+        sexOwner.setText(owner.getSex().toString());
+        datebirthOwner.setText(owner.getDatebirth().toString());
+        addressOwner.setText(owner.getAddress());
+        cityOwner.setText(owner.getCity());
+        codFiscalOwner.setText(owner.getFiscalCode());
+        emailOwner.setText(owner.getEmail());
+    }
+
+    public String getIdOwner() {
+        return idOwner;
+    }
+
+    public String getIdPet() {
+        return idPet;
+    }
+}
